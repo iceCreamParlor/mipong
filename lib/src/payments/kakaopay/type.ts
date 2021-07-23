262b961d6191de54522fee6dfb1c5423b1811d69
Heejae-Kim <heejjeeh@gmail.com>
2021-07-23 23:11:01 +0900

    7/23 작업

--- lib/src/payments/kakaopay/type.ts
+++ lib/src/payments/kakaopay/type.ts
@@ -70,8 +70,19 @@ export interface KakaoPayReadyResponse {
 export type KakaoPayRegisterSubscriptionParam = KakaoPayReadyParam;
 export type KakaoPayRegisterSubscriptionResponse = KakaoPayReadyResponse;
 export interface KakaoPayApproveParam {
-  orderCid: string;
-  pgToken: string;
+  // 결제 고유번호, 결제 준비 API 응답에 포함
+  tid: string;
+  // 가맹점 주문번호, 결제 준비 API  요청과 일치해야 함
+  partner_order_id: string;
+  // 가맹점 회원 id, 결제 준비 API 요청과 일치해야 함
+  partner_user_id: string;
+  // 결제 승인 요청을 인증하는 토큰.
+  // 사용자 결제 수단 선택 완료 시, approval_url로 redirection해줄 때 pg_token을 query string으로 전달
+  pg_token: string;
+  // 결제 승인 요청에 대해 저장하고 싶은 값, 최대 200자
+  payload?: string;
+  // 상품 총액, 결제 준비 API 요청과 일치해야 함
+  total_amount?: number;
 }
 export interface KakaoPayApproveResponse {
   // 요청 고유 번호
