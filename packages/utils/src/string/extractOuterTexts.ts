c2b5d292c928c4ed4351b6276545c2faeb925f29
김희재 <heejae.kim@toss.im>
2022-07-07 17:57:25 +0900

    feat: extractOuterTexts 구현

---
+++ packages/utils/src/string/extractOuterTexts.ts
@@ -0,0 +1,34 @@
+/**
+ * 지정한 lefts, right 텍스트를 [포함하는] 사이의 텍스트를 반환한다.
+ *
+ * @example
+ * extractOuterTexts('동해물과 백두산이 마르고 닳도록, 백두산이 손발이 닳도록', '백두산이', '닳도록')
+ * // return ['백두산이 마르고 닳도록', '백두산이 손발이 닳도록']
+ * @param target 가공 대상 텍스트.
+ * @param left 검색 시작 텍스트
+ * @param right 검색 종료 텍스트, 원하는 output 텍스트 내부에 동일한 right가 포함되지 않도록 주의해야 한다.
+ */
+export function extractOuterTexts(
+  target: string,
+  left: string,
+  right: string
+): string[] {
+  const extractedTexts = [];
+  let offset = 0;
+
+  while (target.includes(left) && target.includes(right)) {
+    const startIdx = target.indexOf(left, offset);
+    const endIdx = target.indexOf(right, startIdx);
+
+    if (startIdx === -1 || endIdx === -1) {
+      break;
+    }
+
+    const extractedText = target.substring(startIdx, endIdx + right.length);
+
+    extractedTexts.push(extractedText);
+    offset = endIdx + right.length;
+  }
+
+  return extractedTexts;
+}
--- packages/utils/src/string/index.ts
+++ packages/utils/src/string/index.ts
@@ -1,1 +1,2 @@
 export * from "./extractInnerText";
+export * from "./extractOuterTexts";
