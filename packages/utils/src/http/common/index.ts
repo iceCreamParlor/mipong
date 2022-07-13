d2d457a02c6b56f795e02a8e09b87de4ea52233f
김희재 <heejae.kim@toss.im>
2022-07-13 21:49:49 +0900

    feat: rc.4

--- packages/utils/src/http/common/index.ts
+++ packages/utils/src/http/common/index.ts
@@ -1,4 +1,6 @@
+export * from "./CommonCookieManager";
 export * from "./CommonCookieParser";
 export * from "./CommonCookieStore";
 export * from "./CommonHttpClient";
 export * from "./CommonHttpTemplate";
+export * from "./createCommonHttpTemplate";
